60b5c3d4b47840fac10930c63f06569d6157a616
Anatoliy K <778966@mail.ru>
2022-09-20 12:04:00 +0500

    added examp2 test0

---
+++ Example2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Напиши любое число: ");
+
+int array = int.Parse(Console.ReadLine());
+
+int [] UserArray = new int[]{array};
+{
+    int lenght = UserArray.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        Console.WriteLine(UserArray[2]);
+        index++;
+    }
+    
+}
+
+
+//if (user_number > 100 && user_number < 1000) Console.Write("Вторя цифра этого числа = " + user_number /1 % 1);
+
+if (array < 100) Console.WriteLine("Третьей цифры нет!");
+
+
+
