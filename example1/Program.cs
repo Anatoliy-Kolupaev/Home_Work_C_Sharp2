b2f5b739da33222b2801601459dec0993cc73fbc
Anatoliy K <778966@mail.ru>
2022-09-20 11:27:55 +0500

    added examp1

--- example1/Program.cs
+++ example1/Program.cs
@@ -3,6 +3,7 @@
 int user_number = int.Parse(Console.ReadLine());
 
 if (user_number > 100 && user_number < 1000) Console.Write("Вторя цифра этого числа = " + user_number /10 % 10);
+
 else System.Console.WriteLine("Ты ввел не правельное число! По пробуй еще раз!");
 
 
