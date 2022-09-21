9b53a413f21aad495ce9284f520b2858b9076943
Anatoliy <778966@mail.ru>
2022-09-21 16:49:49 +0500

    Final ok

--- Final/Program.cs
+++ Final/Program.cs
@@ -49,11 +49,10 @@ while(n < v)
     void task1()
     {
         Console.Write("Напиши любое трехзначное число: ");
-        int user_number = int.Parse(Console.ReadLine());
-        if (user_number > 100 || user_number < 1000) Console.WriteLine("Вторя цифра этого числа = " + user_number /10 % 10); 
+        int user_number = int.Parse(Console.ReadLine());        
+        if (user_number > 100 && user_number < 1000) Console.WriteLine("Вторя цифра этого числа = " + user_number /10 % 10); 
         else System.Console.WriteLine("Ты ввел не правельное число! Необходимо ввести ТРЕХЗНАЧНОЕ число!");
-        if (user_number == 0) return ;
-  
+        123  
     }       
     
     void task2()
